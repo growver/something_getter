8b537c48aee0a16ec031aea95f7f533936b01051
growver <code.recivers@gmail.com>
2019-04-21 22:49:45 +0900

    コメントをつけて見やすくなったはず.

--- main.py
+++ main.py
@@ -19,24 +19,37 @@ import requests
 from time import sleep
 
 def download_img(url, file_name):
+    #画像をダウンローします.
+    #クワシイコトハワカンナイ
+    #curl的な何かだと思われる.
     r = requests.get(url, stream=True)
     if r.status_code == 200:
         with open(file_name, 'wb') as f:
             f.write(r.content)
 
 def getter(url):
-    url="https://store.line.me/stickershop/product/13758/ja"
+    #URLからURLを抜き出します
+    
+    #この辺ではページのHTMLソースを取得しています.
     res = requests.get(url)
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, "html.parser")
+    
+    #URLが含まれているタグは<li>だったので,<li>の要素を全てelemsに入れます
     elems = soup.select('li')
     l=[]
     result=[]
     for elem in elems:
+        #elemsの中には3通りのurlがある.
+        #iPhone用　android用　なんかちっこいの
+        #iphone用となんかちっこいのは画質とurlの末尾のみが異なる
+        #なんかちっこいのは<div>タグで簡単に取得できる.
         l.append(str(elem.select('div')))
         for a in l:
             tex=a
             l2=[]
+            
+            #ターゲットURLは()でサンドイッチなので()の間の文字をリストに加える.
             for var in range(0,len(tex)):
                 if tex[var]=="(":
                     var=var+1
@@ -45,14 +58,22 @@ def getter(url):
                         var=var+1
                 else:
                     pass
+                
+            #リストを文字列に直す
             text=""
             for x in l2:
                 text += x
+                
+            #末尾をいじって なんかちっこいの=>iphone用 とする.
             text=text.replace("sticker_key@2x.png", "sticker@2x.png")
+            
+            #URLが載っていない要素は何も返さないのでここで捨てる.
             if len(text)==0:
                 pass
             else:
                 result.append(text)
+                
+    #なぜかわからんけど同じ要素がたくさん出現してくどいから消す.
     return list(set(result))
 
 def saver(URL,savedir):
