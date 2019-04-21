49b2c8e7280e560cacf88423734f185ebd6d54d2
growver <code.recivers@gmail.com>
2019-04-22 00:49:57 +0900

    for_windows.pyは誤字がひどかったので修正.
    どうやらwindowsではフォルダの管理者が厳格なのかわかりにくいのか.
    Pythonから開くことができません.
    
    開けるようになったらこのコードは使えます.

--- for_windows.py
+++ for_windows.py
@@ -8,9 +8,10 @@ Created on Mon Apr 22 00:34:16 2019
 
 import cv2
 import os
-def color_changer(dirpath,exdir):
-    f=os.listdir(dirpath)
-    f=[fa for a in f if ".jpg" in a]
-    img = cv2.imread(dirpath+"//"+f)
-    img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    cv2.imwrite(exdir+"//"+f)
+def color_changer(sourcedir,exitdir):
+    f=os.listdir(sourcedir)
+    f=[f for a in f if ".jpg" in a]
+    for path in f:
+    	img = cv2.imread(sourcedir+"//"+path)
+    	img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    	cv2.imwrite(exitdir+"//"+path)
